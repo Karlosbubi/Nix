3dd7e1da2a2ad99eced8ec9a580734ff339ae741
Kurt Brunner <Kontakt@Karlosbubi.de>
2024-01-28 17:58:01 +0100

    Formar

--- Workstation/Desktop/Gnome1.nix
+++ Workstation/Desktop/Gnome1.nix
@@ -1,23 +1,23 @@
 { config, pkgs, ... }:
 
 {
-# Enable the X11 windowing system.
-services.xserver.enable = true;
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
-# Enable the GNOME Desktop Environment.
-services.xserver.displayManager.gdm.enable = true;
-services.xserver.desktopManager.gnome.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
-environment.variables.EDITOR = "gnome-text-editor";
+  environment.variables.EDITOR = "gnome-text-editor";
 
-# Configure keymap in X11
-services.xserver = {
-  layout = "de";
-  xkbVariant = "";
-};
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "de";
+    xkbVariant = "";
+  };
 
-environment.systemPackages = with pkgs; [
-  # Placeholder
-];
+  environment.systemPackages = with pkgs; [
+    # Placeholder
+  ];
 
 }
