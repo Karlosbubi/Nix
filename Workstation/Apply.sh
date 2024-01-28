e64d2ee4da38482c7488be60da3f330b9d0dc9ea
Kurt Brunner <Kontakt@Karlosbubi.de>
2024-01-28 17:35:04 +0100

    typo and remove test pkg

--- Workstation/Apply.sh
+++ Workstation/Apply.sh
@@ -7,7 +7,7 @@ sudo rm -rf /etc/nixos/Desktop/*
 sudo mkdir -p /etc/nixos/Desktop
 sudo cp $HOME/Projects/Nix/Workstation/Desktop/* /etc/nixos/Desktop/
 
-# Base NixConf ans Switch
+# Base NixConf and Switch
 sudo rm /etc/nixos/configuration.nix
 sudo cp $HOME/Projects/Nix/Workstation/configuration.nix /etc/nixos/configuration.nix
 sudo nixos-rebuild switch
